9aac01326cc33ca869cab1ec87e0e98064614613
AmilAbdullazadeh <amilva@code.edu.az>
2019-12-26 18:12:04 +0400

    yeah first commit

---
+++ src/components/Display.jsx
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+// import { Link } from 'react-router';
+import Nav from './Nav';
+// import { isLoggedIn } from '../utils/AuthService';
+import { CloudinaryContext, Video } from 'cloudinary-react';
+import axios from 'axios';
+import { Share } from 'react-twitter-widgets'
+
+class Display extends Component {
+
+    state = { videos: [] };
+
+    getVideos() {
+        axios.get('http://res.cloudinary.com/localhostreact/video/list/miniflix.json')
+            .then(res => {
+                console.log(res.data.resources);
+                this.setState({ videos: res.data.resources});
+            });
+    }
+
+    componentDidMount() {
+        this.getVideos();
+    }
+
+    render() {
+
+        const { videos }  = this.state;
+
+        return (
+            <div>
+                <Nav />
+                <h3 className="text-center"> Latest Videos on Miniflix </h3>
+                <hr/>
+
+                <div className="col-sm-12">
+                    <CloudinaryContext cloudName="unicodeveloper">
+                        { videos.map((data, index) => (
+                            <div className="col-sm-4" key={index}>
+                                <div className="embed-responsive embed-responsive-4by3">
+                                    <Video publicId={data.public_id} width="300" height="300" controls/>
+                                </div>
+                                <div> Created at {data.created_at} </div>
+                                <Share url={`http://res.cloudinary.com/unicodeveloper/video/upload/${data.public_id}.mp4`} />
+                            </div>
+                        ))
+                        }
+                    </CloudinaryContext>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Display;
